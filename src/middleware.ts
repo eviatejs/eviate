af7b7e58f8d5a8c0170e424cff2ff20d561de145
janaSunrise <warriordefenderz@gmail.com>
2022-08-16 23:06:00 +0530

    Add middleware basic functionality.

---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { MiddlewarePosition } from './enums/MiddlewarePosition';
+
+import type { Context } from './context';
+
+interface MiddlewareHandler {
+  (ctx: Context): Context | Promise<Context>;
+}
+
+export class Middleware {
+  private before: MiddlewareHandler[] = [];
+  private after: MiddlewareHandler[] = [];
+
+  // Register a middleware
+  public register(position: MiddlewarePosition, handler: MiddlewareHandler) {
+    switch (position) {
+      case MiddlewarePosition.Before:
+        this.before.push(handler);
+        break;
+      case MiddlewarePosition.After:
+        this.after.push(handler);
+        break;
+    }
+  }
+
+  // Run the before middleware functions
+  private async runBefore(ctx: Context): Promise<Context> {
+    for (const handler of this.before) {
+      ctx = await handler(ctx);
+    }
+
+    return ctx;
+  }
+
+  // Run the after middleware functions
+  private async runAfter(ctx: Context): Promise<Context> {
+    for (const handler of this.after) {
+      ctx = await handler(ctx);
+    }
+
+    return ctx;
+  }
+}
