4eca99dbdaa27de9fe5c39682c3aab83f1f4c1f8
apoorvcodes <tofu.develops@gmail.com>
2022-09-13 23:10:57 +0530

    feat: router map done

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,3 +1,19 @@
+import { Context } from './context';
+import { Tree } from './tree';
+
 export class router {
-  constructor() {}
+  public notFound: Context;
+  public hanlder: Context;
+  public routes: [];
+  constructor() {
+    this.routes = new Map([
+      ['GET', new Tree()],
+      ['POST', new Tree()],
+      ['OPTIONS', new Tree()],
+      ['HEAD', new Tree()],
+      ['PUT', new Tree()],
+      ['DELETE', new Tree()],
+      ['PATCH', new Tree()]
+    ]);
+  }
 }
