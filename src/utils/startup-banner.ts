41d702f7522dd2564e915af1f21a95bd4e3a71e5
janaSunrise <warriordefenderz@gmail.com>
2022-09-23 22:38:17 +0530

    js/ts display done

--- src/utils/startup-banner.ts
+++ src/utils/startup-banner.ts
@@ -4,20 +4,24 @@ import gradient from 'gradient-string';
 
 import packageJson from '../../package.json';
 
-// Map os.type() to a frieldy name
 const osTypeMap: Record<string, any> = {
   Darwin: 'macOS',
-  Linux: 'Linux',
-  Windows_NT: 'Windows'
+  Linux: 'linux',
+  Windows_NT: 'windows'
 };
 
+const isTypescript =
+  'typescript' in packageJson.dependencies ||
+  'typescript' in packageJson.devDependencies;
+const language = isTypescript ? 'typescript' : 'javascript';
+
 const banner = `
-   ____     _      __     
-  / __/  __(_)__ _/ /____ 
+   ____     _      __
+  / __/  __(_)__ _/ /____
  / _/| |/ / / _ / __/ -_)
 /___/|___/_/\_,_/\__/\__/
 
-v${packageJson.version} | Running on ${osTypeMap[os.type()]}
+v${packageJson.version} (${language}) | Running on ${osTypeMap[os.type()]}
 `;
 
 export const startupBanner = () => {
