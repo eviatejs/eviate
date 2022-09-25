180569e2b532aae1d598b66f8504f2879344d237
apoorvcodes <tofu.develops@gmail.com>
2022-09-25 15:58:30 +0530

    feat: update plugin construct

--- src/core/plugin/plugin.ts
+++ src/core/plugin/plugin.ts
@@ -1,12 +1,31 @@
-import type { Context } from '../context';
-import type { handler } from '../../interfaces';
 import { Plugin, PluginSettings } from 'eviate-plugin';
 export class EviatePlugin {
   private plugin: Map<string, Plugin>;
-  private pluginSettings: PluginSettings[];
+  private pluginSettings: Map<string, PluginSettings>;
 
   constructor() {
     this.plugin = new Map();
-    this.pluginSettings = [];
+    this.pluginSettings = new Map();
+  }
+
+  public setPlugin(plugin: Plugin) {
+    this.setPluginSettings(plugin.pluginMetaData.title, plugin.settings);
+    this.plugin.set(plugin.pluginMetaData.title, plugin);
+  }
+
+  public getPlugin(title: string): Plugin | undefined {
+    return this.plugin.get(title);
+  }
+
+  public getAllPlugins(): Map<string, Plugin> {
+    return this.plugin;
+  }
+
+  private setPluginSettings(title: string, settings: PluginSettings) {
+    this.pluginSettings.set(title, Plugin);
+  }
+
+  public getSettings(title: string): PluginSettings | undefined {
+    return this.pluginSettings.get(title);
   }
 }
